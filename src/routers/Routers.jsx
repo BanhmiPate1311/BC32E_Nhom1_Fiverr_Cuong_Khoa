1a8bb004a57256c87013b47d61d4dbc76fdf38a2
BanhmiPate1311 <huynhcuong@gmail.com>
2022-11-09 22:01:32 +0700

    upload project

---
+++ src/routers/Routers.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+
+const Routers = () => {
+  const routing = useRoutes([]);
+  return routing;
+};
+
+export default Routers;
